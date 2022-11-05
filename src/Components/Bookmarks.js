b76fbe82635e59401c080c57f4b545eaafa9c118
Ametza Maldonado <96350702+ametzamaldonado@users.noreply.github.com>
2022-11-04 20:48:07 -0400

    Update Bookmarks.js

--- src/Components/Bookmarks.js
+++ src/Components/Bookmarks.js
@@ -1,8 +1,26 @@
 import { useState } from "react";
 import Bookmark from "./Bookmark";
+import axios from "axios";
+// ^ this is our new package for making API calls
+
+// INDEX Page??
+
+const API = process.env.REACT_APP_API_URL;
+console.log(API)
+// request for data must come AFTER component is loaded to the DOM
+// otherwise we have to RACE condition - page might be done before data arrives;
 
 function Bookmarks() {
-  const [bookmarks] = useState([]);
+  const [ bookmarks, setBookmarks ] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API}/bookmarks`) // fetches data from API that is running as well
+      .then((res) => { setBookmarks(response.data)}) // saves data to our hook
+      .catch((err) => {console;log(err)})
+  }, []) // <------ empty dependencies array
+  // when the dependency array of a useEffect is empty the code inside will only run once: after the component mounts!
+  // adding a value to our dependency array will cause our useEffect to run anytime that value changes
+
   return (
     <div className="Bookmarks">
       <section>
