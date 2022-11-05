e2b574ef3381c065899c47104ba3e9433d1ac083
Ametza Maldonado <96350702+ametzamaldonado@users.noreply.github.com>
2022-11-04 20:45:17 -0400

    Update BookmarkDetails.js

--- src/Components/BookmarkDetails.js
+++ src/Components/BookmarkDetails.js
@@ -1,12 +1,40 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+// SHOW PAGE!!
+const API = process.env.REACT_APP_API_URL;
 
 function BookmarkDetails() {
-  const [bookmark] = useState([]);
-  let { index } = useParams();
+  const [ bookmark, setBookmark ] = useState({});
+  let { index } = useParams(); // this is whatever the value of our id in the params is
 
-  useEffect(() => {}, []);
-  const handleDelete = () => {};
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`${API}/bookmarks/${index}`)
+      .then((res) => setBookmark(res.data))
+      .catch((err) => navigate(`/404`)) // this could navigate to any route that we didn't define really...but we should navigate to something appropriate we don't have like `/404` or `/error`
+  }, [ index ]);
+  /* 
+  We need to GET the bookmark from the DB to display our user
+    1. Get the INDEX it exists at in the db
+    2. GET the data
+    3. Display it to our user in state;
+  */
+  const handleDelete = () => {
+    axios.delete(`${API}/bookmarks/${index}`) // deletes
+      .then(res => navigate(`/bookmarks`)) // ...then returns to other bookmarks here!!!
+      .catch(err => console.log(err))
+  };
+
+  /* 
+  We need to send a DELETE request to our DB
+    1. Get the INDEX of our bookmark
+    2. Send the DELETE request to our API
+    3. < what do we need to do after this works? >
+  */
   return (
     <article>
       <h3>
